d79dfc51c6426a5fa0d55381c2df11e28bcfc4f0
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-20 22:46:21 -0300

    Header e container de filmes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,21 @@
+import CardContainer from "../components/CardContainer";
+import MovieCard from "../components/MovieCard";
+import movies from "../data/movies.json"
+
 export default function Home(){
     return(
-        <h1> Home Page</h1>
+        <>
+            <CardContainer titulo="Filmes Antigos">
+                {
+                    movies
+                    .filter(filme =>(
+                        filme.ano_lancamento < 2000
+                    ))
+                    .map(filme =>(
+                        <MovieCard key={filme.id} {...filme}/>
+                    ))
+                }
+            </CardContainer>
+        </>
     )
 }
