67c49d22e706f0d4a9cdf3716dec3c62d95f59d4
Joao Pedro <signorjoaopedro@gmail.com>
2024-11-08 12:50:00 -0300

    Alteração no h1

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -2,7 +2,7 @@ export default function CardContainer({titulo, children}) {
     return(
         <>
         <div className="mx-5 my-5">
-            <h1 className="pb-2">{titulo}</h1>
+            <h1 className="pb-2 text-2xl font-bold">{titulo}</h1>
             <div className="flex flex-wrap justify-between">
                 {children}
             </div>
